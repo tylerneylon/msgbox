2df9a94191b48042366f909920262f7dbb1d4eb0
Tyler Neylon <tylerneylon@gmail.com>
2014-04-17 18:22:49 -0700

    Update ctest, add a broken test that needs fixing.
    This is highlighting problems sending longer messages via tcp. I think
    there are actually two bugs: (1) the message length field in the header
    can only indicate lengths < 64k; and (2) continue_recv has a bug where
    it doesn't advance the data pointer in the waiting buffer.

--- test/msgbox_test.c
+++ test/msgbox_test.c
@@ -13,6 +13,8 @@
 
 #include "ctest.h"
 
+#include <errno.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -49,7 +51,146 @@ int udp_port;
 int tcp_port;
 
 ///////////////////////////////////////////////////////////////////////////////
-// server
+// long string test
+
+char *long_string = NULL;
+
+void init_long_string_if_needed() {
+  if (long_string) return;
+
+  const long len = 1 << 20;
+  char *alpha   = "abcdefghijklmnopqrstuvwxyz";
+  long_string   = malloc(len);
+  char *end     = long_string + len;
+  for (char *c = long_string; c < end; ++c) {
+    long index = c - long_string;
+    index      = (int)(sin(index) * 12.5 + 12.5);
+    *c         = alpha[index];
+  }
+  long_string[len - 1] = '\0';
+}
+
+int long_string_server_done;
+
+void long_string_server_update(msg_Conn *conn, msg_Event event, msg_Data data) {
+  test_printf("Server: Received event %s\n", event_names[event]);
+  if (event == msg_error) test_printf("Server: Error: %s\n", msg_as_str(data));
+
+  if (event == msg_message) {
+    test_str_eq(msg_as_str(data), long_string);
+
+    // Send a message back to trigger the client to close.
+    // We avoid a TIME_WAIT state on the server when the client closes.
+    msg_Data data = msg_new_data("thanks!");
+    msg_send(conn, data);
+    msg_delete_data(data);
+  }
+
+  if (event == msg_connection_closed) {
+    test_printf("Server: Connection closed.\n");
+    long_string_server_done = true;
+  }
+
+  if (event == msg_connection_lost) {
+    test_failed("Server: connection lost.\n");
+  }
+}
+
+int long_string_server() {
+  long_string_server_done = false;
+
+  char address[256];
+  snprintf(address, 256, "tcp://*:%d", tcp_port);
+
+  msg_listen(address, msg_no_context, long_string_server_update);
+  int timeout_in_ms = 10;
+  while (!long_string_server_done) msg_runloop(timeout_in_ms);
+
+  // Sleep for 1ms as the client expects to finish before the server.
+  // (Early server termination could be an error, so we check for it.)
+  usleep(1000);
+
+  return test_success;
+}
+
+int long_string_client_done;
+
+void long_string_client_update(msg_Conn *conn, msg_Event event, msg_Data data) {
+  test_printf("Client: Received event %s\n", event_names[event]);
+  if (event == msg_error) test_printf("Client: Error: %s\n", msg_as_str(data));
+
+  if (event == msg_connection_ready) {
+    test_printf("long_string has len=%d\n", strlen(long_string));
+    msg_Data data = msg_new_data(long_string);
+    msg_send(conn, data);
+    msg_delete_data(data);
+  }
+
+  if (event == msg_message) {
+    test_printf("Client: closing connection.\n");
+    msg_disconnect(conn);
+  }
+
+  if (event == msg_connection_closed) {
+    test_printf("Client: Connection closed.\n");
+    long_string_client_done = true;
+  }
+}
+
+int long_string_client(pid_t server_pid) {
+  long_string_client_done = false;
+
+  // Sleep for 1ms to give the server time to start.
+  usleep(1000);
+
+  char address[256];
+  snprintf(address, 256, "tcp://127.0.0.1:%d", tcp_port);
+
+  msg_connect(address, msg_no_context, long_string_client_update);
+  int timeout_in_ms = 10;
+  while (!long_string_client_done) {
+    msg_runloop(timeout_in_ms);
+
+    // Check to see if the server process ended before we expected it to.
+    int status;
+    if (!long_string_client_done && waitpid(server_pid, &status, WNOHANG)) {
+      test_failed("Client: Server process ended before client expected.");
+    }
+  }
+
+  return test_success;
+}
+
+int long_string_test() {
+  init_long_string_if_needed();
+
+  pid_t child_pid = fork();
+  if (child_pid == -1) test_failed("fork: %s\n", strerror(errno));
+
+  if (child_pid == 0) {
+    // Child process.
+    exit(long_string_server());
+  } else {
+    // Parent process.
+    test_printf("Client pid=%d  server pid=%d\n", getpid(), child_pid);
+    int client_failed = long_string_client(child_pid);
+    int server_status;
+    wait(&server_status);
+    int server_failed = WEXITSTATUS(server_status);
+
+    // Help check for memory leaks.
+    test_that(net_allocs_for_class(0) == 0);
+
+    test_printf("Test: client_failed=%d server_failed=%d.\n", client_failed, server_failed);
+
+    return client_failed || server_failed;
+  }
+  return test_success;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// basic server
 
 int server_done;
 int server_event_num;
@@ -102,7 +243,7 @@ int server(int protocol_type) {
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-// client
+// basic client
 
 int client_done;
 int client_event_num;
@@ -210,6 +351,6 @@ int main(int argc, char **argv) {
   tcp_port = rand() % 1024 + 1024;
 
   start_all_tests(argv[0]);
-  run_tests(udp_test, tcp_test);
+  run_tests(udp_test, tcp_test, long_string_test);
   return end_all_tests();
 }
