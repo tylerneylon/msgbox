1e0a4c062507c1075a5356ec85829cc564faf95b
Tyler Neylon <tylerneylon@gmail.com>
2014-10-13 20:35:08 -0700

    Switch msgbox_now to a header-only module.
    This avoids using up the common symbold "now" in the global linkage
    namespace.

--- msgbox/msgbox_now.h
+++ msgbox/msgbox_now.h
@@ -5,9 +5,32 @@
 // A cross-platform way to get a high resolution
 // timestamp.
 //
+// This is a header-only file in order to support
+// static linkage, which avoids polluting the
+// global linkage namespace with the "now" symbol.
+//
+// The "now" function returns the number of seconds
+// since the start of 1970 as a floating-point value.
+//
 
 #pragma once
 
-// Returns the number of seconds since the start of 1970
-// as a floating-point value.
-double now();
+#ifdef _WIN32
+
+// windows version
+static double now() {
+  // TODO
+}
+
+#else
+
+#include <sys/time.h>
+
+// mac/linux version
+static double now() {
+  struct timeval t;
+  gettimeofday(&t, NULL);  // 2nd param = optional time zone pointer.
+  return (double)t.tv_sec + 1e-6 * (double)t.tv_usec;
+}
+
+#endif
